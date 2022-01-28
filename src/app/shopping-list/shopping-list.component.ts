32c1f9d98e0ad2cbdbb46c9e93a9e0ae7ff4bbcc
Ishan Sharma <ishansharma1320@gmail.com>
2022-01-28 18:00:24 +0530

    Section-3 complete

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredients.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('I1',4),
+    new Ingredient('I2',7),
+  ]
   constructor() { }
 
   ngOnInit(): void {
