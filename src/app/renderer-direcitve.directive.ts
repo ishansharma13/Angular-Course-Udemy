22e1b1a53ab5d2ee6f87f2853a763ffae5497390
Ishan Sharma <ishansharma1320@gmail.com>
2022-01-30 15:40:04 +0530

    binding to properties defined inside a custom directive

--- src/app/renderer-direcitve.directive.ts
+++ src/app/renderer-direcitve.directive.ts
@@ -1,15 +1,20 @@
-import { Directive,Renderer2,OnInit, ElementRef,HostListener, HostBinding } from '@angular/core';
+import { Directive,Renderer2,OnInit, ElementRef,HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appRendererDirecitve]'
 })
 export class RendererDirecitveDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
-  @HostBinding('style.color') color: string = 'black';
-  constructor(private elRef: ElementRef,private renderer: Renderer2) { }
+  @Input() defaultBackground: string = 'yellow';
+  @Input() defaultTextColor: string = 'red';
+  @HostBinding('style.backgroundColor') backgroundColor: string ;
+  @HostBinding('style.color') color: string ;
+  constructor(private elRef: ElementRef,private renderer: Renderer2) { 
+
+  }
 
   ngOnInit(): void {
-      
+      this.backgroundColor = this.defaultBackground;
+      this.color = this.defaultTextColor;
   }
   @HostListener('mouseenter') mouseOver(){
     // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
@@ -20,7 +25,7 @@ export class RendererDirecitveDirective implements OnInit {
   @HostListener('mouseleave') mouseleave(){
     // this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
     // this.renderer.setStyle(this.elRef.nativeElement,'color','black');
-    this.backgroundColor = 'transparent';
-    this.color = 'black';
+    this.backgroundColor = this.defaultBackground;
+    this.color = this.defaultTextColor;
   }
 }
