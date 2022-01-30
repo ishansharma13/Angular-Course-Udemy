209e95381973eb23da6c4a4b81f1a98b3e0af6fa
Ishan Sharma <ishansharma1320@gmail.com>
2022-01-30 15:24:24 +0530

    HostBinding: changing the value for attributes of host without renderer

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ __ One cannot have more than one structural directive on a single element __
 ## Creating a custom directive
   Use renderer to make DOM manipulation instead of changing the ElementRef directly
 
+** HostListener: when you want to listen to events and change something on a directive, should be used with renderers **
+** HostBinding: an easire way to achieve the same (HostListener) **
+
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
 ## Code scaffolding
--- src/app/renderer-direcitve.directive.ts
+++ src/app/renderer-direcitve.directive.ts
@@ -1,21 +1,26 @@
-import { Directive,Renderer2,OnInit, ElementRef,HostListener } from '@angular/core';
+import { Directive,Renderer2,OnInit, ElementRef,HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appRendererDirecitve]'
 })
 export class RendererDirecitveDirective implements OnInit {
-  
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.color') color: string = 'black';
   constructor(private elRef: ElementRef,private renderer: Renderer2) { }
 
   ngOnInit(): void {
       
   }
   @HostListener('mouseenter') mouseOver(){
-    this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
-    this.renderer.setStyle(this.elRef.nativeElement,'color','white');
+    // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+    // this.renderer.setStyle(this.elRef.nativeElement,'color','white');
+    this.backgroundColor = 'blue';
+    this.color = 'white';
   }
   @HostListener('mouseleave') mouseleave(){
-    this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
-    this.renderer.setStyle(this.elRef.nativeElement,'color','black');
+    // this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement,'color','black');
+    this.backgroundColor = 'transparent';
+    this.color = 'black';
   }
 }
