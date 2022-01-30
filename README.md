b83ecb9b2583c1df7e6408bbbba5b71b2a6aaab0
Ishan Sharma <ishansharma1320@gmail.com>
2022-01-30 19:35:07 +0530

    Adding logging and Accounts service and integrating with angular components

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Boiler Branch
+# Section-9
 
-## Checkout this branch when changing projects or use some of the dependencies from this branch to resolve dependency conflicts
+## Services may contain code that may be centralised and can be used by one or multiple components
+
+## A Service is just a normal typescript class
+
+## One may expect if service is a normal TS class, that may also mean that inorder to use it in angular compoents, we just have to import and initialise a service class and then call its methods, this will work, however it is not a correct or to say a recommended way to do so
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountsService } from '../shared/accounts.service';
+import { LoggingService } from '../shared/logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-
+ 
+  constructor(private loggingService: LoggingService,private accountsService: AccountsService){}
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
+    this.accountsService.addAccount({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logToConsole(accountStatus);
   }
 }
