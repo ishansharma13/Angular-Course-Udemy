045af52d321a5d12a0d7a7e6107efb276bed29b9
Ishan Sharma <ishansharma1320@gmail.com>
2022-01-30 15:16:53 +0530

    Using HostListeners

--- src/app/renderer-direcitve.directive.ts
+++ src/app/renderer-direcitve.directive.ts
@@ -1,14 +1,21 @@
-import { Directive,Renderer2,OnInit, ElementRef } from '@angular/core';
+import { Directive,Renderer2,OnInit, ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appRendererDirecitve]'
 })
 export class RendererDirecitveDirective implements OnInit {
-
+  
   constructor(private elRef: ElementRef,private renderer: Renderer2) { }
 
   ngOnInit(): void {
-      this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
-      this.renderer.setStyle(this.elRef.nativeElement,'color','white');
+      
+  }
+  @HostListener('mouseenter') mouseOver(){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+    this.renderer.setStyle(this.elRef.nativeElement,'color','white');
+  }
+  @HostListener('mouseleave') mouseleave(){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+    this.renderer.setStyle(this.elRef.nativeElement,'color','black');
   }
 }
